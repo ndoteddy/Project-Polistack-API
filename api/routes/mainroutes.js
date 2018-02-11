33e600d2b4da5c999376daf0fa46cb9beb1953e8
ndoteddy <nando.teddy@gmail.com>
2018-02-11 09:45:38 +0700

    v0.0.2 - fix main route variable

--- api/routes/mainroutes.js
+++ api/routes/mainroutes.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(app) {
     var event = require('../controllers/event_controller');
-    var scrappingmovie = require('../controllers/event_scrapper_controller');
+    var scrappingExternalEvent = require('../controllers/event_scrapper_controller');
     // to event controller (mongo database data)
     app.route('/event')
         .get(event.list_all_events)
@@ -9,5 +9,5 @@ module.exports = function(app) {
 
     // to scrapping controller (external website data)
     app.route('/event/scrapping')
-        .get(scrappingmovie.list_all_event_scrapping);
+        .get(scrappingExternalEvent.list_all_event_scrapping);
 };
